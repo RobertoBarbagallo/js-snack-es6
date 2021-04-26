04952624bec1d53f91c23894535842625d1e789f
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-26 17:23:28 +0200

    consegna finale

--- myPlug.js
+++ myPlug.js
@@ -52,28 +52,106 @@ for (let index = 0; index < bikes.length; index++) {
 
 console.log(arrayDeiPesi);
 
-var valoreMinimo = Infinity
 
-function minOfArray(array) {
-    for (let index = 0; index < array.length; index++) {
-        let element = array[index]
+var valoreMinimo = arrayDeiPesi[0]
+valoreMinimo = newMinOfArray(arrayDeiPesi)
 
-        
-        for (let indice = 0; indice < array.length; indice++) {
-            let elementTwo = array[indice];
-
-            if(element < elementTwo && element < valoreMinimo){
-                valoreMinimo = element
-            }else if (element > elementTwo && elementTwo < valoreMinimo){
-                valoreMinimo = elementTwo
-            }else if (element === elementTwo && element < valoreMinimo){
-                valoreMinimo = element
-            }    
-        }           
+function newMinOfArray (array){
+    for (let index = 1; index < array.length; index++) {
+        const element = array[index];
+        if(element < valoreMinimo){
+            valoreMinimo = element
+        }
     }
+    console.log(valoreMinimo)
+
 }
 
-minOfArray(arrayDeiPesi)
 
-console.log(valoreMinimo)
+// var valoreMinimo = Infinity
+
+// function minOfArray(array) {
+//     for (let index = 0; index < array.length; index++) {
+//         let element = array[index]
+
+        
+//         for (let indice = 0; indice < array.length; indice++) {
+//             let elementTwo = array[indice];
+
+//             if(element < elementTwo && element < valoreMinimo){
+//                 valoreMinimo = element
+//             }else if (element > elementTwo && elementTwo < valoreMinimo){
+//                 valoreMinimo = elementTwo
+//             }else if (element === elementTwo && element < valoreMinimo){
+//                 valoreMinimo = element
+//             }    
+//         }           
+//     }
+// }
+
+// minOfArray(arrayDeiPesi)
+
+// console.log(valoreMinimo)
+
+
+const teams = [
+
+    {
+        nome: "Real",
+        puntiFatti : 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Juve",
+        puntiFatti : 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Milan",
+        puntiFatti : 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Atletico Madrid",
+        puntiFatti : 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Borussia Dortmund",
+        puntiFatti : 0,
+        falliSubiti: 0
+    },
+   
+];
+
+function randomNum(minNum, maxNum) {
+    var rndNum = parseInt((Math.random() * maxNum) + minNum)
+    return rndNum
+}
+
+
+for (let index = 0; index < teams.length; index++) {
+    const squadra = teams[index];
+
+    squadra.puntiFatti = randomNum(1, 12)
+    squadra.falliSubiti = randomNum (1, 100)
+    
+}
+
+console.log(teams)
+
+
+for (let index = 0; index < teams.length; index++) {
+    const object = teams[index];
+   
+
+    const {nome, falliSubiti} = object
+
+
+    console.log(nome, falliSubiti)
+    
+}
+
+
+
 
